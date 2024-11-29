993b1dadb6a3fff0c7e8a1fad5cce40c5aed213a
Samuel <goodlifesam@gmail.com>
2024-11-29 11:25:33 -0800

    something

--- server/go.mod
+++ server/go.mod
@@ -1,19 +1,24 @@
-module videoGameApp
+module video-game-server
 
 go 1.23.1
 
 require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
-	github.com/dimuska139/rawg-sdk-go v1.0.1 // indirect
-	github.com/gofiber/fiber/v3 v3.0.0-beta.3 // indirect
-	github.com/gofiber/utils/v2 v2.0.0-beta.7 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/dimuska139/rawg-sdk-go/v3 v3.0.0
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.57.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,47 +1,73 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
-	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dimuska139/rawg-sdk-go/v3"
 )
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 
 func main() {
 	fmt.Println("Hello world")
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	// Retrieve API key from .env
+	apiKey := goDotEnvVariable("RAWG_API_KEY")
+	if apiKey == "" {
+		log.Fatal("RAWG_API_KEY is not set in the environment")
+	}
+
+	config := rawg.Config{
+		ApiKey:   apiKey, // Your personal API key (see https://rawg.io/apidocs)
+		Language: "ru",
+		Rps:      5,
+	}
+	client := rawg.NewClient(http.DefaultClient, &config)
+
+	filter := rawg.NewGamesFilter().
+		SetSearch("Gta5").
+		SetPage(1).
+		SetPageSize(10).
+		ExcludeCollection(1).
+		WithoutParents()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Millisecond*500))
+	defer cancel()
+	data, total, err := client.GetGames(ctx, filter)
+	if err != nil {
+		log.Fatalf("Error fetching games: %v", err)
+	}
+
+	app.Get("/games", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"total": total,
+			"games": data,
+		})
+	})
+
 	log.Fatal(app.Listen(":4000"))
-
 }
-
-// import (
-//     "context"
-//     "fmt"
-//     "net/http"
-//     "strings"
-//     "time"
-//     "github.com/dimuska139/rawg-sdk-go/v3"
-// )
-
-// func main() {
-//     config := rawg.Config{
-//         ApiKey:  "yourapikey", // Your personal API key (see https://rawg.io/apidocs)
-//         Language: "ru",
-//         Rps:      5,
-//     }
-//     client := rawg.NewClient(http.DefaultClient, &config)
-
-//     filter := rawg.NewGamesFilter().
-//         SetSearch("Gta5").
-//         SetPage(1).
-//         SetPageSize(10).
-//         ExcludeCollection(1).
-//         WithoutParents()
-
-//     ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Millisecond*500))
-//     defer cancel()
-//     data, total, err := client.GetGames(ctx, filter)
-
-//     ...
-// }
