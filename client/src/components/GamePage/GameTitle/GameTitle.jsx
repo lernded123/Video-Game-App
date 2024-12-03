9039cc39a910e865f92bc88c6788037d7a83960b
Samuel <goodlifesam@gmail.com>
2024-12-03 10:17:14 -0800

    added assets

---
+++ client/src/components/GamePage/GameTitle/GameTitle.jsx
@@ -0,0 +1,10 @@
+import gameTitle from "../../../assets/CyberPunk/CPLogo.jpg"
+import React from 'react'
+
+const GameTitle = () => {
+  return (
+    <div>GameTitle</div>
+  )
+}
+
+export default GameTitle
