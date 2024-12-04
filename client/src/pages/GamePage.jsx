a3efe42f621dc80d3e601ad667be6993fd12ce89
Samuel <goodlifesam@gmail.com>
2024-12-04 10:07:10 -0800

    added logic to game page

---
+++ client/src/pages/GamePage.jsx
@@ -0,0 +1,10 @@
+import GameTitle from "../components/GamePage/GameTitle/GameTitle"
+import React from 'react'
+
+const GamePage = () => {
+  return (
+    <div>GamePage</div>
+  )
+}
+
+export default GamePage
