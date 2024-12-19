a3505568df01f03198990558ad811015ef6d02fc
Samuel <goodlifesam@gmail.com>
2024-12-19 10:25:29 -0800

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from "./components/Navbar/Navbar"
 import HeroCarousel from './components/MainCarousel/Carousel'
 import GameCarousel from './components/GameCarousel/GameCarousel'
+// import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 function App() {
   
